ac6ceda2436713c74e2fe7058550f3f49cee2aa3
Ron Reynolds <ron@ronreynolds.com>
2026-09-14 19:39:28 -0700

    chore(Beeps): just saving this class for future reference; easier than digging thru the git history (IMHO)

---
+++ app/src/main/java/com/ronreynolds/android/clock/Beeps.java
@@ -0,0 +1,44 @@
+package com.ronreynolds.android.clock;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.util.Log;
+
+/**
+ * just to keep track of this code; might come in handy someday :)
+ */
+public class Beeps {
+    private final String LOG_TAG = getClass().getSimpleName();
+
+    // old A-440 beep
+    private void playTone() {
+        Log.d(LOG_TAG, "playTone");
+        int sampleRate = 44100;
+        int durationMs = 200;
+        int numSamples = durationMs * sampleRate / 1000;
+
+        double[] samples = new double[numSamples];
+        for (int i = 0; i < numSamples; i++) {
+            samples[i] = Math.sin(2 * Math.PI * 440 * i / sampleRate);
+        }
+
+        byte[] buffer = new byte[numSamples];
+        for (int i = 0; i < numSamples; i++) {
+            buffer[i] = (byte) (samples[i] * 127);
+        }
+
+        AudioTrack track = new AudioTrack(
+                AudioManager.STREAM_MUSIC,
+                sampleRate,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_8BIT,
+                buffer.length,
+                AudioTrack.MODE_STATIC
+        );
+
+        track.write(buffer, 0, buffer.length);
+        track.play();
+    }
+
+}
