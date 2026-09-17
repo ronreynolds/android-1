b707cc092f4e180229129dcd1b1b4c55d9a186d9
Ron Reynolds <ron@ronreynolds.com>
2026-09-17 10:21:02 -0700

    feature(settings): added observers, switched Log to Logs, added setters and new getters, moved keys into constants

--- app/src/main/java/com/ronreynolds/android/clock/Settings.java
+++ app/src/main/java/com/ronreynolds/android/clock/Settings.java
@@ -3,8 +3,11 @@ package com.ronreynolds.android.clock;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
+import com.ronreynolds.android.util.Logs;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,8 +15,21 @@ import java.util.concurrent.TimeUnit;
  * within MainActivity.onCreate
  */
 public class Settings {
+    public interface SettingObserver {
+        void onPeriodChange();
+        void onClockTypeChange();
+    }
+
     private static final String LOG_TAG = "Settings";
+    private static final List<SettingObserver> observerList = new ArrayList<>();
+    private static final String KEY_24HR_TIME = "is_24hr_time";
+    private static final String KEY_MINUTE_PERIOD = "minute_period";
+
     private static SharedPreferences preferences;
+
+    public static void addObserver(SettingObserver obs) {
+        observerList.add(obs);
+    }
 
     /**
      * initialize the Settings; otherwise you get only default values back
@@ -26,37 +42,41 @@ public class Settings {
 
     public static boolean getBoolean(String key, boolean defaultVal) {
         boolean value = preferences != null ? preferences.getBoolean(key, defaultVal) : defaultVal;
-        Log.d(LOG_TAG, key + " = " + value);
+        Logs.d(LOG_TAG, key + " = " + value);
         return value;
     }
 
     public static int getInteger(String key, int defaultValue) {
         String valueAsString = getString(key, null);
         if (valueAsString == null) {
-            Log.w(LOG_TAG, "unable to find value for key " + key);
+            Logs.w(LOG_TAG, "unable to find value for key " + key);
             return defaultValue;
         }
         try {
             return Integer.parseInt(valueAsString);
         } catch (Exception fail) {
-            Log.e(LOG_TAG, "failure to convert " + valueAsString + " to int", fail);
+            Logs.e(LOG_TAG, "failure to convert " + valueAsString + " to int", fail);
             return defaultValue;
         }
     }
 
     public static String getString(String key, String defaultValue) {
         String value = preferences != null ? preferences.getString(key, defaultValue) : defaultValue;
-        Log.d(LOG_TAG, key + " = " + value);
+        Logs.d(LOG_TAG, key + " = " + value);
         return value;
     }
 
     // special helpers to keep keys in one place
     public static boolean is24HrTime() {
-        return getBoolean("is_24hr_time", true);
+        return getBoolean(KEY_24HR_TIME, true);
     }
 
     public static long getPeriodMillis() {
-        return TimeUnit.MINUTES.toMillis(getInteger("minute_period", 1));
+        return TimeUnit.MINUTES.toMillis(getInteger(KEY_MINUTE_PERIOD, 1));
+    }
+
+    public static int getPeriodMinutes() {
+        return getInteger(KEY_MINUTE_PERIOD, 1);
     }
 
     public static float getPositiveFloat(String key, float defaultVal) {
@@ -72,4 +92,17 @@ public class Settings {
         return Math.max(0.1f, value);
     }
 
+    public static void setPeriodMinutes(int minutes) {
+        preferences.edit().putInt(KEY_MINUTE_PERIOD, minutes).apply();
+        for (SettingObserver observer : observerList) {
+            observer.onPeriodChange();
+        }
+    }
+
+    public static void setUse24HourTime(boolean h24) {
+        preferences.edit().putBoolean(KEY_24HR_TIME, h24).apply();
+        for (SettingObserver observer : observerList) {
+            observer.onClockTypeChange();
+        }
+    }
 }
