76a11625234a1f554be3be86094761809aba4d8f
Ron Reynolds <ron@ronreynolds.com>
2026-09-17 10:20:03 -0700

    feature(main-activity): lots of code cleanup, move-al, and removal; added observers for setting changes, handlers for new buttons and log-list; etc

--- app/src/main/java/com/ronreynolds/android/clock/MainActivity.java
+++ app/src/main/java/com/ronreynolds/android/clock/MainActivity.java
@@ -3,47 +3,21 @@ package com.ronreynolds.android.clock;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Calendar;
+import com.ronreynolds.android.util.Logs;
+import com.ronreynolds.android.util.Time;
 
 /**
  * main class for the application
  */
 public class MainActivity extends AppCompatActivity {
     private final String LOG_TAG = getClass().getSimpleName();
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu resource
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            // Launch your settings screen
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-        if (id == R.id.action_quit) {
-            shutdown();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     /**
      * invoked when the app is first created
@@ -55,16 +29,100 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(LOG_TAG, "onCreate called");
+        Logs.d(LOG_TAG, "onCreate called");
         super.onCreate(savedInstanceState);
-        Settings.init(this);    // initialize the global application settings
-        scheduleRepeatEvent();
-        setContentView(R.layout.activity_main);
 
+        Settings.init(this);    // initialize the global application settings
+
+        scheduleRepeatEvent();
+
+        // register out listener for Setting changes
+        Settings.addObserver(new Settings.SettingObserver() {
+            @Override
+            public void onPeriodChange() {
+                scheduleRepeatEvent();  // overwrite scheduled event when the period is changed
+            }
+
+            @Override
+            public void onClockTypeChange() {
+                // we don't care about this here
+            }
+        });
+
+        // create the GUI bits
+        setContentView(R.layout.activity_main);
+        setupLogView();
+        setupSettingsGUI();
+        setupButtons();
+    }
+
+    private void setupLogView() {
+        // our Log view - updated as log events occur
+        TextView logView = findViewById(R.id.logView);
+        Logs.addObserver((int level, String context, String message, Throwable ex) -> {
+            String timestamp = Time.getTimeNow();
+            char cLevel = Logs.levelToChar(level);
+            logView.append(String.format("%s %s %c \"%s\"%n", timestamp, context, cLevel, message));
+        });
+    }
+
+    private void setupSettingsGUI() {
+        // setup Settings controls
+        RadioGroup periodGroup = findViewById(R.id.periodGroup);
+        periodGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            int periodMinutes;
+            if (checkedId == R.id.period1) {
+                periodMinutes = 1;
+            } else if (checkedId == R.id.period5) {
+                periodMinutes = 5;
+            } else if (checkedId == R.id.period10) {
+                periodMinutes = 10;
+            } else if (checkedId == R.id.period15) {
+                periodMinutes = 15;
+            } else {
+                Logs.wtf(LOG_TAG, "invalid period minutes; checkedId:" + checkedId);
+                periodMinutes = 1;  // default to every minute (even tho this should never ever happen)
+            }
+            Settings.setPeriodMinutes(periodMinutes);
+        });
+        switch (Settings.getPeriodMinutes()) {
+            case 1:
+                periodGroup.check(R.id.period1);
+                break;
+            case 5:
+                periodGroup.check(R.id.period5);
+                break;
+            case 10:
+                periodGroup.check(R.id.period10);
+                break;
+            case 15:
+                periodGroup.check(R.id.period15);
+                break;
+            default:
+                Logs.w(LOG_TAG, "period not set or invalid - " + Settings.getPeriodMillis());
+                Settings.setPeriodMinutes(1);
+        }
+
+        RadioGroup timeFormatGroup = findViewById(R.id.timeFormatGroup);
+        timeFormatGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            Settings.setUse24HourTime(checkedId == R.id.time24);
+        });
+        if (Settings.is24HrTime()) {
+            timeFormatGroup.check(R.id.time24);
+        } else {
+            timeFormatGroup.check(R.id.time12);
+        }
+    }
+
+    private void setupButtons() {
+        // simple "go" button (mostly for testing but also to init the TTS system)
         Button btn = findViewById(R.id.btnSayTime);
         btn.setOnClickListener(v -> {
             startService(new Intent(this, ToneService.class));   // API 23
         });
+        // clean quit option
+        Button quitBtn = findViewById(R.id.btnQuit);
+        quitBtn.setOnClickListener(v -> shutdown());
     }
 
     private void scheduleRepeatEvent() {
@@ -77,31 +135,16 @@ public class MainActivity extends AppCompatActivity {
                 new Intent(this, ToneReceiver.class),   // target for messages
                 PendingIntent.FLAG_UPDATE_CURRENT // if PendingIntent already exists update it
         );
-        final long startTimeMillis = getMillisTillNextMinute();
+        final long startTimeMillis = Time.getMillisTillNextMinute();
         // start the alarms flowing
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, startTimeMillis, periodMillis, operation);
-        Log.d(LOG_TAG, "alarmManager.setRepeating returned; " +
+        Logs.d(LOG_TAG, () -> "alarmManager.setRepeating returned; " +
                 "starting in " + startTimeMillis / 1000 + " seconds with period "
                 + periodMillis / 1000);
     }
 
-    private static long getMillisTillNextMinute() {
-        // before Oreo java-time wasn't available
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LocalDateTime nextMinute = LocalDateTime.now().withSecond(0).withNano(0).plusMinutes(1);
-            return nextMinute.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-        } else {
-            Calendar nextMinute = Calendar.getInstance();
-            // Truncate seconds and millis and add 1 minute
-            nextMinute.set(Calendar.SECOND, 0);
-            nextMinute.set(Calendar.MILLISECOND, 0);
-            nextMinute.add(Calendar.MINUTE, 1);
-            return nextMinute.getTimeInMillis();
-        }
-    }
-
     private void shutdown() {
-        Log.d(LOG_TAG, "shutdown called");
+        Logs.d(LOG_TAG, "shutdown called");
 
         // Cancel repeating alarm
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
