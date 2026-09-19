ca1cd47a994a2656ad27bb3265b6a122b2b409c8
Ron Reynolds <ron@ronreynolds.com>
2026-09-18 17:06:28 -0700

    feature: include indentity-hash in logs to determine if there are multiple service instances (so far no); changed timing between events from scheduled at fixed frequency to fixed delay (immediately schedule next event when event is received) - this has improved event timing noticeably; some refactoring of code to make methods cleaner and smaller

---
+++ app/src/main/java/com/ronreynolds/android/clock/SpeechService.java
@@ -0,0 +1,180 @@
+package com.ronreynolds.android.clock;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.speech.tts.TextToSpeech;
+
+import androidx.core.app.NotificationCompat;
+
+import com.ronreynolds.android.util.Logs;
+import com.ronreynolds.android.util.Time;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * the service that actually provides the sound output
+ */
+public class SpeechService extends Service {
+    private static final String NOTIFICATION_CHANNEL_ID = "talk_clock_channel";
+    private static final String NOTIFICATION_CHANNEL_NAME = "Talking Clock";
+    private static SimpleDateFormat timeFormat;
+
+    private final String LOG_TAG = getClass().getSimpleName();
+    private TextToSpeech textToSpeech;
+    private volatile boolean ttsReady = false;
+    private Handler handler;    // used to delay TTS messages until it's ready to speak
+    private Notification startNotification;
+
+    /**
+     * @return the communication channel to this service; null since we have none
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    /**
+     * much like the ctor but better (because we actually have a context to pass)
+     */
+    @Override
+    public void onCreate() {
+        Logs.d(LOG_TAG, "onCreate; id:" + System.identityHashCode(this));
+        super.onCreate();
+        handler = new Handler(Looper.getMainLooper());  // used for async callback until TTS is ready
+
+        Settings.addObserver(new Settings.SettingObserver() {
+            @Override
+            public void onPeriodChange() {
+                // if the period changes we discard the currently-scheduled intent and create a new one
+                cancelPendingIntent();
+                scheduleNextIntent();
+            }
+
+            @Override
+            public void onClockTypeChange() {
+                updateTimeFormat();
+            }
+        });
+
+        // set our time-format from Settings before we use it (else Bad Things happen)
+        updateTimeFormat();
+        Logs.d(LOG_TAG, "onCreate; creating TextToSpeech");
+        textToSpeech = new TextToSpeech(this, status -> {
+            if (status == TextToSpeech.SUCCESS) {
+                Logs.d(LOG_TAG, "TTS initialized");
+                ttsReady = true;    // this tells us it's safe to use the TTS
+            } else {
+                Logs.e(LOG_TAG, "TTS init failed");
+            }
+        });
+
+        // only needed on Oreo (Android-8.0) and later
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            getSystemService(NotificationManager.class)
+                    .createNotificationChannel(new NotificationChannel(
+                            NOTIFICATION_CHANNEL_ID,
+                            NOTIFICATION_CHANNEL_NAME,
+                            NotificationManager.IMPORTANCE_LOW
+                    ));
+        }
+
+        startNotification = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setContentTitle("Talking Clock Running")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .build();
+    }
+
+    /**
+     * called every time an intent/event is sent to the service.
+     *
+     * @param intent  The Intent supplied to {@link android.content.Context#startService},
+     *                as given.  This may be null if the service is being restarted after
+     *                its process has gone away, and it had previously returned anything
+     *                except {@link #START_STICKY_COMPATIBILITY}.
+     * @param flags   Additional data about this start request.
+     * @param startId A unique integer representing this specific request to
+     *                start.  Use with {@link #stopSelfResult(int)}.
+     * @return START_STICKY tells caller to keep this service alive between calls
+     */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Logs.d(LOG_TAG, "onStartCommand; id:" + System.identityHashCode(this));
+        scheduleNextIntent();   // schedule the next intent before we do anything else (minimal lag)
+        startForeground(1, startNotification);
+        sayTime();
+        return START_STICKY;    // keep us around after this Intent has been processed
+    }
+
+    @Override
+    public void onDestroy() {
+        Logs.d(LOG_TAG, "onDestroy");
+        cancelPendingIntent();
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();   // <-- THIS unbinds the ServiceConnection
+            textToSpeech = null;
+        }
+        super.onDestroy();
+    }
+
+    private PendingIntent createIntent() {
+        Context context = this;
+        int requestCode = 42;
+        Intent messageWithTarget = new Intent(this, IntentRelay.class);
+        return PendingIntent.getBroadcast(context, requestCode, messageWithTarget,
+                PendingIntent.FLAG_UPDATE_CURRENT   // update if one already exists with this context + id
+        );
+    }
+
+    /**
+     * create and schedule the next Intent to invoke this service
+     */
+    private void scheduleNextIntent() {
+        // create these before delay-till-next-minute calc to minimize edge-case near minute boundary
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        // round down to the edge of the minute
+        long now = System.currentTimeMillis();
+        long triggerTime = Time.roundDownToMinuteMillis(now + Settings.getPeriodMillis());
+        // schedule the intent to fire in periodMillis
+        alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, triggerTime, createIntent());
+        Logs.d(LOG_TAG, () -> "scheduleNextIntent; alarmManager.setAndAllowWhileIdle returned; " +
+                "firing in " + (triggerTime - now) / 1000 + " seconds");
+    }
+
+    private void cancelPendingIntent() {
+        Logs.d(LOG_TAG, "cancelling pending intent");
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.cancel(createIntent());
+    }
+
+    private void sayTime() {
+        if (!ttsReady) {
+            Logs.d(LOG_TAG, "TTS not ready; adding recursive delayed callback");
+            handler.postDelayed(this::sayTime, 100);    // call us back in 100ms
+        } else {
+            String text;
+            final SimpleDateFormat currentTimeFormat = timeFormat;
+            synchronized (currentTimeFormat) { // because SimpleDateFormat isn't thread-safe
+                text = currentTimeFormat.format(new Date());
+            }
+            Logs.d(LOG_TAG, "sayTime - " + text);
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "SpeechService.sayTime");
+        }
+    }
+
+    private void updateTimeFormat() {
+        timeFormat = new SimpleDateFormat(Settings.is24HrTime() ? "H m" : "h m a", Locale.US);
+    }
+}
