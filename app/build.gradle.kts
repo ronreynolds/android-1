4892d4100a2af6aa9797737c3dc6891a0ef6b22e
Ron Reynolds <ron@ronreynolds.com>
2026-09-20 15:52:57 -0700

    build: bumped app version from 1.0.1 to 1.0.2 and JVM version from 11 to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23 // ZTE Z981 (Android 6.0)
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -26,8 +26,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
